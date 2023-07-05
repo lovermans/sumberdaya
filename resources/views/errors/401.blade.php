77c44abf11852031358ec7cd662d4168f99b7193
lovermans <ilma.adzim@gmail.com>
2023-07-05 21:22:08 +0700

    Perkaya Pesan Kesalahan

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,11 +1,12 @@
 @extends('rangka')
 
 @section('isi')
-    <div class="pesan-internal">
-        {{-- <p>{{__('Unauthorized')}}.</p> --}}
-        <p>{{ $exception->getMessage() ?: 'Periksa akses masuk dan durasi sesi.' }}</p>
-        <a class="utama" href="{{ $urlRangka->route('mulai') }}">OKE</a>
-        <a class="sekunder" href="#" onclick="event.preventDefault();this.parentElement.remove()">TUTUP</a>
-        <div class="bersih"></div>
-    </div>
+<div class="pesan-internal">
+    {{-- <p>{{__('Unauthorized')}}.</p> --}}
+    <p>{{ $exception->getMessage() ?: 'Bisa jadi belum mendapat akses masuk atau durasi akses masuk sudah habis. Cobalah
+        masuk kembali.' }}</p>
+    <a class="utama" href="{{ $urlRangka->route('mulai') }}">OKE</a>
+    <a class="sekunder" href="#" onclick="event.preventDefault();this.parentElement.remove()">TUTUP</a>
+    <div class="bersih"></div>
+</div>
 @endsection
--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,11 +1,14 @@
 @extends('rangka')
 
 @section('isi')
-    <div class="pesan-internal">
-        {{-- <p>{{__('Forbidden')}}.</p> --}}
-        <p>{{ $exception->getMessage() ? $exception->getMessage().' Periksa ijin akses dan durasi sesi' : 'Periksa ijin akses dan durasi sesi.' }}</p>
-        <a class="utama" href="{{ $urlRangka->route('mulai') }}">OKE</a>
-        <a class="sekunder" href="#" onclick="event.preventDefault();this.parentElement.remove()">TUTUP</a>
-        <div class="bersih"></div>
-    </div>
+<div class="pesan-internal">
+    {{-- <p>{{__('Forbidden')}}.</p> --}}
+    <p>{{ $exception->getMessage() ? $exception->getMessage().' Bisa jadi akses dibatasi untuk menjalankan fungsi ini
+        atau durasi akses masuk sudah habis. Cobalah minta akses ke Administrator atau masuk kembali' : 'Bisa jadi akses
+        dibatasi untuk menjalankan fungsi ini atau durasi akses masuk sudah habis. Cobalah minta akses ke Administrator
+        atau masuk kembali.' }}</p>
+    <a class="utama" href="{{ $urlRangka->route('mulai') }}">OKE</a>
+    <a class="sekunder" href="#" onclick="event.preventDefault();this.parentElement.remove()">TUTUP</a>
+    <div class="bersih"></div>
+</div>
 @endsection
