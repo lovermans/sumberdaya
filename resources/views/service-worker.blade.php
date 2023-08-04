5ba13334cd9856f2280694d4720df73016c17cc6
lovermans <ilma.adzim@gmail.com>
2023-08-04 16:53:30 +0700

    Kecilkan Skala Huruf

--- resources/views/service-worker.blade.php
+++ resources/views/service-worker.blade.php
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 202308041620;
+var CACHE_VERSION = 202308041653;
 var CURRENT_CACHES = {
     prefetch: "{{ $confRangka->get('app.name', 'Laravel') }}-cache-v" + CACHE_VERSION
 };
