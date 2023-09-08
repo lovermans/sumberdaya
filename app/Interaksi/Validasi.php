e3834a4d73235487164b98b2f7d8db951abd7da3
lovermans <ilma.adzim@gmail.com>
2023-09-08 13:48:13 +0700

    Perbaiki Kesalahan Validasi Tambah Data Pengaturan

--- app/Interaksi/Validasi.php
+++ app/Interaksi/Validasi.php
@@ -19,11 +19,11 @@ class Validasi
             $permintaan,
             [
                 '*.kata_kunci' => ['sometimes', 'nullable', 'string'],
-                '*.atur_jenis.*' => ['sometimes', 'nullable', 'string', 'max:20',],
+                '*.atur_jenis.*' => ['sometimes', 'nullable', 'string', 'max:20'],
                 '*.atur_butir.*' => ['sometimes', 'nullable', 'string', 'max:40'],
                 '*.atur_status.*' => ['sometimes', 'nullable', 'string', Rule::in(['', 'AKTIF', 'NON-AKTIF'])],
                 '*.bph' => ['sometimes', 'nullable', 'numeric', Rule::in([25, 50, 75, 100])],
-                '*.urut.*' => ['sometimes', 'nullable', 'string']
+                '*.urut.*' => ['sometimes', 'nullable', 'string'],
             ],
             static::pesanValidasiArrayPengaturan()
         );
@@ -37,8 +37,8 @@ class Validasi
                 '*.atur_butir' => ['required', 'string', 'max:40', Rule::unique('aturs')->where(function ($query) use ($permintaan) {
                     $query->where('atur_jenis', $permintaan[0]['atur_jenis']);
                 })],
-                '*.atur_id_pembuat' => ['reqquired', 'string', 'exists:sdms,sdm_no_absen'],
-                ...static::dasarValidasiPengaturan()
+                '*.atur_id_pembuat' => ['required', 'string', 'exists:sdms,sdm_no_absen'],
+                ...static::dasarValidasiPengaturan(),
             ],
             static::pesanValidasiArrayPengaturan()
         );
@@ -53,7 +53,7 @@ class Validasi
                     $query->where('atur_jenis', $permintaan[0]['atur_jenis'])->whereNot('atur_uuid', $uuid);
                 })],
                 '*.atur_id_pengubah' => ['required', 'string', 'exists:sdms,sdm_no_absen'],
-                ...static::dasarValidasiPengaturan()
+                ...static::dasarValidasiPengaturan(),
             ],
             static::pesanValidasiArrayPengaturan()
         );
@@ -72,7 +72,7 @@ class Validasi
             '*.atur_id_pengunggah.*' => 'ID Pengunggah urutan ke-:position wajib berupa karakter dan terdaftar.',
             '*.atur_id_pembuat.*' => 'ID Pembuat urutan ke-:position wajib berupa karakter dan terdaftar.',
             '*.atur_id_pengubah.*' => 'ID Pengubah urutan ke-:position wajib berupa karakter dan terdaftar.',
-            '*.atur_diunggah.*' => 'Waktu Unggah urutan ke-:position wajib berupa tanggal.'
+            '*.atur_diunggah.*' => 'Waktu Unggah urutan ke-:position wajib berupa tanggal.',
         ];
     }
 
@@ -90,10 +90,10 @@ class Validasi
         return static::validasiUmum(
             $permintaan,
             [
-                'atur_unggah' => ['required', 'mimetypes:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
+                'atur_unggah' => ['required', 'mimetypes:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'],
             ],
             [
-                'atur_unggah.*' => 'Berkas yang diunggah wajib berupa file excel (.xlsx).'
+                'atur_unggah.*' => 'Berkas yang diunggah wajib berupa file excel (.xlsx).',
             ]
         );
     }
@@ -106,7 +106,7 @@ class Validasi
                 '*.atur_butir' => ['required', 'string', 'max:40'],
                 '*.atur_id_pengunggah' => ['required', 'string', 'max:10', 'exists:sdms,sdm_no_absen'],
                 '*.atur_diunggah' => ['sometimes', 'nullable', 'date'],
-                ...static::dasarValidasiPengaturan()
+                ...static::dasarValidasiPengaturan(),
             ],
             static::pesanValidasiArrayPengaturan()
         );
